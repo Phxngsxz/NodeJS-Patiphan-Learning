1cbafae9d378bc7b960150ff8a1825d4520198f8
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-16 14:44:35 +0700

    log host pathname search

--- server.js
+++ server.js
@@ -4,15 +4,27 @@ let url = require('url');
 let fs = require('fs');
 const { log } = require('console');
 
-http.createServer(function (req, res){
-    fs.readFile('index.html', function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    })
-}).listen(8000);
+let adr = 'http://localhost:8000/dafault.html?year=2020&month=november';
+let q = url.parse(adr, true);
 
-fs.rename('mynewfile2.txt', 'myrenamefile.text' , function(err){
-    if (err) throw err;
-    console.log('File Deleted!');
-})
+console.log(q.host); // return localhost:8000
+console.log(q.pathname);
+console.log(q.search);
+
+let qdata = q.query;
+console.log(qdata.month);
+
+
+
+// http.createServer(function (req, res){
+//     fs.readFile('index.html', function(err, data){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(data);
+//         return res.end();
+//     })
+// }).listen(8000);
+
+// fs.rename('mynewfile2.txt', 'myrenamefile.text' , function(err){
+//     if (err) throw err;
+//     console.log('File reanemd!');
+// })
