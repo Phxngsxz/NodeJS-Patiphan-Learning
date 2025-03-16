228eb681ac23e4efc66cf40e17c50c7f8561bf80
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-16 14:15:54 +0700

    read yaer and month form query string

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 let http = require('http');
 let dt = require('./myfirstmodule');
+let url = require('url');
 
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write(req.url);
-    res.end();
+    let q = url.parse(req.url, true).query;
+    let txt = q.year + " " + q.month;
+    res.end(txt);
 }).listen(8000);
