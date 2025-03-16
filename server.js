2342243b68981022798312eb4e723bb23cc5a65f
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-16 15:08:06 +0700

    eventEmitter

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ let url = require("url");
 let fs = require("fs");
 const { log } = require("console");
 let uc = require("upper-case");
+let rs = fs.createReadStream('./demo.txt');
+let events = require('events');
+let eventEmitter = new events.EventEmitter();
+
+// Create an event handler:
+let myEventHandler = function(){
+    console.log('I hear scream');
+}
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+// fire the 'scream' event:
+eventEmitter.emit('scream');
 
 // let adr = 'http://localhost:8000/dafault.html?year=2020&month=november';
 // let q = url.parse(adr, true);
@@ -15,13 +28,13 @@ let uc = require("upper-case");
 // let qdata = q.query;
 // console.log(qdata.month);
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(uc.upperCase('Phongsaphon somsin'));
-    res.end();``
-  })
-  .listen(8000);
+// ``http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'})
+//     res.write(uc.upperCase('Phongsaphon somsin'));
+//     res.end();``
+//   })
+//   .listen(8000);``
 
 // fs.rename('mynewfile2.txt', 'myrenamefile.text' , function(err){
 //     if (err) throw err;
