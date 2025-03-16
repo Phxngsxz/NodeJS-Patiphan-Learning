fb5b44d4d2424a0c4a7c2bf9933121015a2e8d2d
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-16 15:01:59 +0700

    use upper-case packgate

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let dt = require("./myfirstmodule");
 let url = require("url");
 let fs = require("fs");
 const { log } = require("console");
+let uc = require("upper-case");
 
 // let adr = 'http://localhost:8000/dafault.html?year=2020&month=november';
 // let q = url.parse(adr, true);
@@ -16,18 +17,9 @@ const { log } = require("console");
 
 http
   .createServer(function (req, res) {
-    let q = url.parse(req.url, true);
-    let filename = "." + q.pathname;
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
-      }
-      
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(uc.upperCase('Phongsaphon somsin'));
+    res.end();``
   })
   .listen(8000);
 
