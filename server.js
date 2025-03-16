f062e9814b3b8fa56d6a05f2381800b11469f183
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-16 14:19:57 +0700

    create index.htlm and show data

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 let http = require('http');
 let dt = require('./myfirstmodule');
 let url = require('url');
+let fs = require('fs');
 
 http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    let q = url.parse(req.url, true).query;
-    let txt = q.year + " " + q.month;
-    res.end(txt);
+    fs.readFile('index.html', function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
 }).listen(8000);
